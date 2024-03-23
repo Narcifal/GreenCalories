ecd155950104d0544fa81bb99ef93ee235317d47
Denys Niestierov <niestierov.denys.natife@gmail.com>
2024-03-23 18:40:27 +0200

    initial commit

---
+++ GreenCalories/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GreenCalories
+//
+//  Created by Denys Niestierov on 23.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
